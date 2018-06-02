59769f3478629361ff48e33e24f8c2cddb238258
Makame Makame <hamzamac@outlook.com>
2018-06-02 20:43:02 +0200

    Added ApiClient class and Newtonsoft package

---
+++ AXIS.Weather/ApiClient.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AXIS.Weather
+{
+    class ApiClient
+    {
+        HttpClient _client; 
+
+        public ApiClient()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri("https://opendata-download-metobs.smhi.se/");
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<JObject> GetDataAsync(string path)
+        {
+            string contentString = "";
+
+            HttpResponseMessage response = await _client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                contentString = await response.Content.ReadAsStringAsync();
+            }
+
+            return JObject.Parse(contentString);
+        }
+    }
+}
--- AXIS.Weather/Program.cs
+++ AXIS.Weather/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,18 @@ namespace AXIS.Weather
 {
     class Program
     {
+        
+        static async Task<string> GetData()
+        {
+
+
+            return await GetDataAsync("api/version/1.0/parameter/1/station/159880/period/latest-months/data.json");
+        }
+
         static void Main(string[] args)
         {
+            Console.Write(GetData().GetAwaiter().GetResult());
+            Console.Read();
         }
     }
 }
